bf0b30a98e56eee19d4e0450e000d32cba3c5a58
Nilesh <nilesh.chavan6080@gmail.com.com>
2024-05-16 23:27:04 +0530

    modified test1.java file by developer2

--- test1.java
+++ test1.java
@@ -2,4 +2,9 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
+
+     public void m1(){
+
+       System.out.println("Add method");
+    }
 }
