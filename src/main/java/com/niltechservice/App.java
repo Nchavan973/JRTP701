8377795d1290b76599d53c6b6cc8667351aefa66
Nilesh <nileshchavan@niltech.com>
2025-01-08 10:16:14 +0530

    m1 method done

--- src/main/java/com/niltechservice/App.java
+++ src/main/java/com/niltechservice/App.java
@@ -6,6 +6,12 @@ public class Customer
     {
        System.out.println("Hello Nilesh");
 	   System.out.println(" Task 420");
-	   
+       System.out.println("Hello Nilesh, how are you");
     }
+	
+	public void m1(){
+		int i=10;
+		int j=20;
+		int c=10
+	}
 }
